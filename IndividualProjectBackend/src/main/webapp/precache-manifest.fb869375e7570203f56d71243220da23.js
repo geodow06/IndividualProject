2d1bb2932d568402bb019066d5c0067011961496
geodow06 <georgedowne06@gmail.com>
2019-02-21 17:43:20 +0000

    Got requests working

---
+++ IndividualProjectBackend/src/main/webapp/precache-manifest.fb869375e7570203f56d71243220da23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "55eb59fe3a0bbb660509",
+    "url": "/static/js/main.55eb59fe.chunk.js"
+  },
+  {
+    "revision": "11b0a4d9ba9c04bc74b7",
+    "url": "/static/js/2.11b0a4d9.chunk.js"
+  },
+  {
+    "revision": "55eb59fe3a0bbb660509",
+    "url": "/static/css/main.9f168d08.chunk.css"
+  },
+  {
+    "revision": "a5963c589e552b3f4c8386023e964991",
+    "url": "/index.html"
+  }
+];
