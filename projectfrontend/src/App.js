f0069e857972c1b85571e54d494409ba5287d5e8
geodow06 <georgedowne06@gmail.com>
2019-02-22 15:43:12 +0000

    updating website

--- projectfrontend/src/App.js
+++ projectfrontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css'; 
 import NavBar from './NavBar.js';
-
+import Cube from './Cube.js';
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
+        <NavBar/> 
+        <Cube/>
       </div>
     );
   }
