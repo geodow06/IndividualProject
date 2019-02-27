9d9af406ae83cc2da42ac0a97b0394a6f0e134c7
geodow06 <georgedowne06@gmail.com>
2019-02-27 00:01:06 +0000

    updating react

--- reactrestart/src/UserGet.js
+++ reactrestart/src/UserGet.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import './App.css'; 
 // import '/roofpig_and_three.min.js'
 import axios from 'axios';
-
+// const users = []; 
+let users = [{username:"hello", password:"password"},{username:"hello2",password:"password2"}]
 class UserGet extends Component { 
   constructor(){ 
     super(); 
     this.state = { 
       data: ["hello","goodbye"], 
+      users:[{username:"hello", password:"password"},{username:"hello2",password:"password2"}], 
       username:"null", 
       password:"null"
     }
+  } 
+ 
+ 
+  user = {username:"hello",password:"goodbye"}
+  userList = () =>{ 
+    const listItems = users.map((user)=> 
+      <li>{user}</li>
+    ); 
+    return( 
+      <ul>{listItems}</ul>
+    );
   }
-
 
 
     getRequest = ()=>{ 
@@ -28,7 +40,10 @@ class UserGet extends Component {
     render() { 
       for(let i=0 ; i<2 ; i++){ 
         return (
-          <div>
+          <div> 
+              {/* <UserGet users={users[0].username}></UserGet>  */}
+              {/* <ul>{this.userList}</ul> */}
+              <p>{users[0].username}</p>
               {/* <button onClick={this.getRequest}>Click</button>  */}
               {/* <p>username={this.state.data[i].userName}</p> 
               <p>password={this.state.data[i].userPassword}</p>  */} 
